d8c5444488fb6cd6ea897a9eed4751302024db0e
AmigoFox <prostoggwpandre@gmail.com>
2024-11-19 15:55:12 +0300

    сдалей таблицу с именамми городов

--- Ai.py
+++ Ai.py
@@ -1,6 +1,11 @@
 import spacy
 nlp = spacy.load("ru_core_news_lg")
 https = "https://rp5.ru/Погода_в_" # дальше для парсинга нужно тут указать город в НУЖНОМ ПАДЕЖЕ
+
+# для определение о каком городе идет речь
+city_text = "city.txt"
+print(city_text)
+
 city = input("Погоду в каком городе хочешь посмотреть ? \n")
 doc = nlp(city)
 for token in doc:
