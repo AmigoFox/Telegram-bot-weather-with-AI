f3ec989ed2934e625735b04fb4d647665f360e91
AmigoFox <prostoggwpandre@gmail.com>
2024-11-20 19:45:08 +0300

    сдалел и объеденил города и ссылки на них. Далее нужно выявить из вопроса какой именно город хочет посмотрить пользователь и указать в таблице ссылку и город

--- Ai.py
+++ Ai.py
@@ -1,10 +1,12 @@
 import spacy
+import pandas as pd
 nlp = spacy.load("ru_core_news_lg")
 https = "https://rp5.ru/Погода_в_" # дальше для парсинга нужно тут указать город в НУЖНОМ ПАДЕЖЕ
 
+name_city = pd.read_csv("name_city.csv")
+print(name_city)
+
 # для определение о каком городе идет речь
-city_text = "links_city.txt"
-print(city_text)
 
 city = input("Погоду в каком городе хочешь посмотреть ? \n")
 doc = nlp(city)
