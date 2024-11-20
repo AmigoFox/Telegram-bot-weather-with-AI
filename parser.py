56c1fbd52db6efa3d2d187c0e80f9c547352cac4
AmigoFox <prostoggwpandre@gmail.com>
2024-11-20 14:36:46 +0300

    сдалей таблицу с именамми городов

--- parser.py
+++ parser.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
-# Инициализация драйвера Selenium
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
@@ -12,31 +10,19 @@ driver = webdriver.Chrome(service=service)
 url = "https://rp5.ru/Погода_в_России"
 driver.get(url)
 
-# Даем время для загрузки контента (можно заменить на явные ожидания, если нужно)
-driver.implicitly_wait(10)
-
-# Получаем HTML-код страницы после загрузки
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-# Поиск всех тегов <a> с атрибутом href
 links = soup.find_all('a', href=True)
 
-# Открытие файла для записи всех ссылок
-with open("city.txt", "a", encoding="utf-8") as file:
-    # Печать и запись всех ссылок
+with open("name_city.txt", "a", encoding="utf-8") as file:
     for link in links:
         href = link['href']
 
-        # Если ссылка не абсолютная, делаем её абсолютной
-        if not href.startswith('http'):
-            href = "https://rp5.ru" + href
+        if href.startswith('/Погода'):
+            city_name = href.split('/')[-1]  # Получаем последнюю часть пути
+            file.write(city_name + "\n")      # Записываем в файл с переводом строки
+            print(city_name)
 
-        # Записываем ссылку в файл
-        file.write(href + "\n")
 
-        # Печатаем ссылку (по желанию)
-        print(href)
-
-# Закрываем браузер
 driver.quit()
