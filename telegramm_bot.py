24cab5a25d611f3e1710b396304b0951e371d5d2
AmigoFox <prostoggwpandre@gmail.com>
2024-11-25 12:44:21 +0300

    пытаюсь сделать бота, чтобы ответил на простой запрос старт и безопасно загрузить токен, чтобы не палить в коде его

---
+++ telegramm_bot.py
@@ -0,0 +1,38 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+from dotenv import load_dotenv
+import os
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+if TOKEN is None:
+    print("Ошибка: BOT_TOKEN не найден в .env файле")
+    sys.exit(1)
+
+dp = Dispatcher()
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}, в каком городе хочешь узнать погоду ?")
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        await message.send_copy(chat_id=TOKEN)
+    except TypeError:
+        await message.answer("Nice try!")
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
