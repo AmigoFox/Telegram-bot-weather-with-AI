8749531d8416667d7becc7ccad19f1179ef1b761
AmigoFox <prostoggwpandre@gmail.com>
2024-11-25 11:50:48 +0300

    Delete req_city.py

--- req_city.py
+++
@@ -1,5 +0,0 @@
-with open("name_city.txt", "r", encoding="utf-8") as infile, open("name_city_new.txt", "w", encoding="utf-8") as outfile:
-  for line in infile:
-    new_line = line.replace("_", " ")
-    outfile.write(new_line)
-    print(new_line)
