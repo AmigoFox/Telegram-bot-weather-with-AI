0959c4d440d7260023877c405eade5c46fb1d8ac
AmigoFox <prostoggwpandre@gmail.com>
2024-11-27 11:50:02 +0300

    сделал обработку смс, теперь надо сохранить в файле и передать ии, который определит какой город

--- telegramm_bot.py
+++ telegramm_bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import sys
+import logging
 from typing import Any
 from aiogram.handlers import MessageHandler
 from dotenv import load_dotenv
@@ -9,34 +10,35 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from pyexpat.errors import messages
 
 load_dotenv("bot.env")
 TOKEN = os.getenv("BOT_TOKEN")
+logging.basicConfig(level=logging.INFO)
 
 if TOKEN is None:
     print("Ошибка: BOT_TOKEN не найден в .env файле")
     sys.exit(1)
 
-dp = Dispatcher()
+
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(bot=bot)
+
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Привет, {html.bold(message.from_user.full_name)}, в каком городе хотите узнать погоду ?")
 
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    try:
-        await message.send_copy(chat_id=TOKEN)
-    except TypeError:
-        await message.answer("Попробуй еще раз.")
-
 async def main() -> None:
     await dp.start_polling(bot)
 
+
+@dp.message()
+async def log_message(message: Message):
+       if not message.from_user.is_bot:  # Проверяем, что сообщение не от бота
+           print(f"Получено сообщение: {message.text} от пользователя {message.from_user.id}")
+       else:
+           print(f"Получено сообщение от бота: {message.text}")
+
 if __name__ == "__main__":
     asyncio.run(main())
-
-class MyHandler(MessageHandler):
-    async def handle(self) -> Any:
-        return self.event.text
